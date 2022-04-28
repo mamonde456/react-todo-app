7e9c44c851f86c994754463ffa115e9ba43f449e
mamonde456 <mamonde456@gmail.com>
2022-04-28 17:01:43 +0900

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RecoilRoot } from "recoil";
+import { ThemeProvider } from "styled-components";
+import App from "./App";
+import { dark } from "./thema";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <RecoilRoot>
+      <ThemeProvider theme={dark}>
+        <App />
+      </ThemeProvider>
+    </RecoilRoot>
+  </React.StrictMode>
+);
